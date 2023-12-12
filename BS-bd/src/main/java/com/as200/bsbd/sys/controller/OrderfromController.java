d51049a22c2c1e2b853cd86d14edeb8166ae3b93
asercn <534304548@qq.com>
2023-12-12 22:44:23 +0800

    修复BUG

--- BS-bd/src/main/java/com/as200/bsbd/sys/controller/OrderfromController.java
+++ BS-bd/src/main/java/com/as200/bsbd/sys/controller/OrderfromController.java
@@ -1,7 +1,14 @@
 package com.as200.bsbd.sys.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.as200.bsbd.common.vo.Result;
+import com.as200.bsbd.sys.entity.Orderfrom;
+import com.as200.bsbd.sys.service.IOrderfromService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
+
+import java.util.List;
 
 /**
  * <p>
@@ -9,10 +16,25 @@ import org.springframework.stereotype.Controller;
  * </p>
  *
  * @author as200
- * @since 2023-12-05
+ * @since 2023-12-12
  */
-@Controller
-@RequestMapping("/sys/orderfrom")
+@RestController
+@RequestMapping("/orderfrom")
+@CrossOrigin
 public class OrderfromController {
+    @Autowired
+    private IOrderfromService orderfromService;
+
+    @ApiOperation("获取所有的订单信息")
+    @GetMapping("/all")
+    public Result<?> getAllOrderform() {
+        List<Orderfrom> data = orderfromService.list();
+        return Result.success(data,"查询成功");
+    }
+
+//    @PostMapping("/add")
+//    public Result<?> addOrder() {
+//
+//    }
 
 }
